4e7a7d2413b7dd1981db1b584553858d8487ddac
ojay8485 <43895959+ojay8485@users.noreply.github.com>
2018-10-22 22:52:10 -0500

    Finished

--- maze.js
+++ maze.js
@@ -1,49 +1,53 @@
-window.onload = function(){
-	var status = document.getElementById("status");
-	
-	var boundary1 = document.getElementById("boundary1");
-	boundary1.onmouseover = function(){
-			boundary1.classList.add("youlose");
-	}
-	
-	var allbounds = document.querySelectorAll(".boundary")
+// JavaScript Document
+var hitwall = true;
 
-	allbounds.forEach(function(bound){
+function checkbounds(bounds){
+	"use strict";
+	bounds.forEach(function(bound){
 		bound.onmouseover = function(){
-			status.innerHTML = "You lose";
-			bound.classList.add("youlose");
-		}
+			bound.setAttribute("class","boundary youlose");
+			hitwall = true;
+			bounds[bounds.length-1].setAttribute("class","boundary example");
+		};
 	});
 	
-	var play= false;
-	var start = document.getElementById("start");
+}
+
+function init(){
+	"use strict";
+	var boundary = document.querySelectorAll(".boundary");
+	checkbounds(boundary);
 	
+	var start = document.getElementById("start");
 	start.onclick = function(){
-		play = true;
-		allbounds.forEach(function(bound){
-				bound.classList.remove("youlose");
-		});
-		status.innerHTML = "Click your mouse on the 'S' to begin";
+		boundary.forEach(function(bound){
+				bound.setAttribute("class","boundary");
+				boundary[boundary.length-1].setAttribute("class","boundary example");
+		 });
+		hitwall = false;
+		document.getElementById("status").innerHTML= "Move throught the maze";
+	};
+	start.onmouseover = function(){
+		hitwall=false;
 	};
 	
-	var end = document.getElementById("end");
-
-	end.onmouseover = function(){
-		if (play === true){
-			flag = false;
-			allbounds.forEach(function(b){
-					if (b.classList.contains("youlose")){
-						flag = true;
-					}
-				});
-			if(flag === false){
-				alert("You win");
-				status.innerHTML = "You win";
-			}
+	var finish = document.getElementById("end");
+	finish.onmouseover = function(){
+		if(hitwall === false){
+			document.getElementById("status").innerHTML= "You Win";
 		}
-	}
+	};
+	finish.onmouseout = function(){document.getElementById("status").innerHTML= "Click S to restart";};
 	
-	document.getElementById("maze").onmouseleave = function(){
-			play = false;
-		}
-};
+	var maze = document.getElementById("maze");
+	maze.onmouseleave = function(){
+		hitwall = true;
+		boundary.forEach(function(bound){
+				bound.setAttribute("class","boundary youlose");
+		 });
+		boundary[boundary.length-1].setAttribute("class","boundary example");
+		
+	};
+}
+
+window.addEventListener("load",init);
